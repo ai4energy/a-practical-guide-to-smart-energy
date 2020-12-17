e2c7f1e2f68e7532861f9abab1e1c2f95d518749
Mingtao Li <mingtao@xjtu.edu.cn>
2020-12-17 16:17:58 +0800

    修改代码让视频居中

--- website/utils.jl
+++ website/utils.jl
@@ -22,6 +22,8 @@ function hfun_bilibili(params)
   cid = params[3]
 
   return """
+  <div style="text-align: center;">
   <iframe src="//player.bilibili.com/player.html?aid=$aid&bvid=$bvid&cid=$cid&page=1&as_wide=1&high_quality=1&danmaku=0&t=0" scrolling="no" border="0" frameborder="no" framespacing="0" width="480" height="360" allowfullscreen="true"></iframe>
+  </div>
   """
 end
